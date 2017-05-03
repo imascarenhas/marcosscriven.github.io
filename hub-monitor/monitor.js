d0150644a40678edeb7148d9f5b200c48a89434e
Marcos Scriven <marcos@scriven.org>
2017-05-03 22:02:49 +0100

    Wait for script to load.

--- hub-monitor/monitor.js
+++ hub-monitor/monitor.js
@@ -1,8 +1,12 @@
 function run() {
-    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js');
-    $("body").html('<canvas id="signalToNoiseRatio" width="400" height="300"></canvas>');
-    getData();
-    createChart('signalToNoiseRatio');
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js').done(
+        () => {
+            document.body.className = '';
+            $("body").html('<canvas id="signalToNoiseRatio" width="400" height="300"></canvas>');
+            getData();
+            createChart('signalToNoiseRatio');
+        }
+    );
 }
 
 function getData() {
